ca1cd593f4ffea9cd422f46a3f5fd90e4cd79f71
JAChelton <joshua.chelton@anyvan.com>
2025-05-27 15:46:05 +0100

    Update Vite configuration

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['axios', 'framer-motion']
+        }
+      }
+    }
+  },
+  preview: {
+    host: true,
+    port: 5173
+  },
+  define: {
+    'process.env': {}
+  }
+})
